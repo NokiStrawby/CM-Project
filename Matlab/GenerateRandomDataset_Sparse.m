89f86ea91715361eb11a53f1c91e0451f1e72c18
Nicolas <manini.nicolas@gmail.com>
2020-09-24 17:08:12 +0200

    Generated Sparse Datasets

--- Matlab/GenerateRandomDataset_Sparse.m
+++ Matlab/GenerateRandomDataset_Sparse.m
@@ -1,29 +1,16 @@
-    %   M       N       Rk      Cnt     k1      k2      k3  ...
+    %   M       N      Cnt     nk  ...
 
 Prelim_Sparse_Batches = { ...
-    genBatch(10, 10, 10, 3, 9, 2, 5); ... %Square Matrices
-    genBatch(20, 20, 20, 3, 19, 2, 9); ...
-    genBatch(40, 40, 40, 3, 39, 2, 9); ...
-    genBatch(80, 80, 80, 3, 79, 2, 9); ...
-    genBatch(160, 160, 160, 3, 159, 2, 9); ...
-    genBatch(320, 320, 320, 3, 319, 2, 9); ...
+    [10, 10, 3, 5]; ... %Square Matrices
+    [20, 20, 3, 5]; ...
+    [40, 40, 3, 5]; ...
+    [80, 80, 3, 5]; ...
+    [160, 160, 3, 5]; ...
+    [320, 320, 3, 5]; ...
     };
 
 Batches = Prelim_Sparse_Batches;
-density = 0.1;
-
-%{
-    e.g. A line containing:
-    [   100,    200,    90,     7,      89,     50 ,    10]
-    Generates a batch consisting of:
-        - 7 Matrices (Cnt)
-        - Each having dimension 100 x 200 (M x N)
-        - Each having rank 90 (Rk)
-        - And asks to give a low rank approximation for each of the ranks:
-            - 89 (k1)
-            - 50 (k2)
-            - 10 (k3)
-%}
+density = 0.60;
     
 outfolder = 'data_random';
 
@@ -31,37 +18,37 @@ for i = 1:size(Batches, 1)
     CurLine = Batches{i};
     M = CurLine(1);
     N = CurLine(2);
-    Rk = CurLine(3);
-    Cnt = CurLine(4);
+    Cnt = CurLine(3);
+    nk = CurLine(4);
     
     for j = 1:Cnt
         fprintf("Generating batch %d, task %d\n", i, j);
         
         % Generate a matrix of given density
         A = full(sprand(M,N,density))*1000;
+        Rk = rank(A);
         
         tic % Compute the svd factorization to derive the optimal approx.
         [U,S,V] = svd(A);
         Time_opt = toc;
         
-        for h = 5:length(CurLine)
-            k = CurLine(h);
-            
+        ks = equalSpaced(Rk-1, 2, nk);
+        
+        for h = 1:length(ks)
+            k = ks(h);
             Ak_opt = U(:, 1:k)*S(1:k,1:k)*V(:,1:k)';
             Ak_delta = norm(A - Ak_opt, 'fro')/norm(A, 'fro');
-            title = sprintf("%s/rnd_%d_%d_%d.mat", outfolder, i, j, (h-4));
+            title = sprintf("%s/srnd_%d_%d_%d.mat", outfolder, i, j, h);
             save(title, 'M', 'N', 'Rk', 'k', 'A', 'Ak_opt', 'Ak_delta', 'Time_opt');
         end
     end
 end
 
-
-function [b] = genBatch(M, N, Rk, Cnt, maxrk, minrk, nk)
+function [b] = equalSpaced(maxrk, minrk, nk)
     step = (maxrk-minrk)/nk;
     step = max(step, 1);
     
     ks = round(minrk:step:maxrk);
     ks(end) = maxrk;
-    
-    b = [M, N, Rk, Cnt, ks];
+    b = ks;
 end
